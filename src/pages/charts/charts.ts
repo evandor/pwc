002ce40d8460cb0dbd5992d43dc3506fb3d0046e
IzzyGray <mira@skysail.io>
2017-02-13 09:37:21 +0100

    renamed tabs

--- src/pages/charts/charts.ts
+++ src/pages/charts/charts.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-charts',
+  templateUrl: 'charts.html'
 })
-export class HomePage {
+export class ChartsPage {
 
   constructor(public navCtrl: NavController) {
 
