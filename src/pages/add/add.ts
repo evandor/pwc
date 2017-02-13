61127065ba21a98cfdcff68914ace0ca2a74ea50
IzzyGray <mira@skysail.io>
2017-02-13 18:13:34 +0100

    add weight

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'page-add',
   templateUrl: 'add.html'
 })
-export class AddPage {
 
-  constructor(public navCtrl: NavController) {
+export class AddPage {
+  
+    myWeight:any;
+    myDate:any;
+    addWeightFormGroup: FormGroup;
+  
+
+  constructor(public navCtrl: NavController,private formBuilder: FormBuilder) {
+
+    this.addWeightFormGroup = this.formBuilder.group({
+      pwcWeight: ['', Validators.required],
+      pwcDate: ['', Validators.required]
+    });
 
   }
+
+  saveWeight(){
+     
+      this.myWeight = this.addWeightFormGroup.value.pwcWeight; 
+      this.myDate = this.addWeightFormGroup.value.pwcDate;
+      document.getElementById("showWeight").innerHTML="Meine Eingabe lautet "+this.myWeight+" am "+this.myDate;
+  }
+    
+
+      
+  
 
 }
