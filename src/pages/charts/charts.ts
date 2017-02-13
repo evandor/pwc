a5e80221179f2a351b72c539e6160e65387928b6
Carsten Graef <carsten@Carstens-MBP.fritz.box>
2017-02-13 20:52:30 +0100

    wip

--- src/pages/charts/charts.ts
+++ src/pages/charts/charts.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+import { Reading } from '../../domain/reading';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-charts',
@@ -8,67 +13,63 @@ import { NavController } from 'ionic-angular';
 })
 export class ChartsPage {
 
-  constructor(public navCtrl: NavController) {
+  private readings: Array<Reading> = new Array();
 
+  public lineChartLegend: boolean = true;
+
+  public lineChartType: string = 'line';
+
+  public lineChartData: Array<any> = [{
+    data: [80,82.3, 81.9],
+    label: 'kg'
+  }];
+
+  newDate(days) {
+    return moment().add(days, 'd');
   }
 
-  // lineChart
-  public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
-  ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    responsive: true
-  };
-  public lineChartColors:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
-  ];
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
- 
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
+  public lineChartLabels: Array<any> = [this.newDate(-4), this.newDate(-3), this.newDate(20)];
+
+  constructor(public navCtrl: NavController, private storage: Storage) {
+    storage.get('readings').then((result) => {
+      console.log("RESULT", result);
+      for (let reading of result) {
+        console.log("Reading: ",reading);
       }
+      console.log("Labels", this.lineChartLabels);
+    });
+  }
+
+  public lineChartOptions: any = {
+    responsive: false,
+    scales: {
+      xAxes: [{
+        type: 'time',
+        time: {
+          displayFormats: {
+          	'millisecond': 'MMM DD',
+            'second': 'MMM DD',
+            'minute': 'MMM DD',
+            'hour': 'MMM DD',
+            'day': 'MMM DD',
+            'week': 'MMM DD',
+            'month': 'MMM DD',
+            'quarter': 'MMM DD',
+            'year': 'MMM DD',
+          }
+        }
+      }],
+    },
+  };
+  public lineChartColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     }
-    this.lineChartData = _lineChartData;
-  }
- 
-  // events
-  public chartClicked(e:any):void {
-    console.log(e);
-  }
- 
-  public chartHovered(e:any):void {
-    console.log(e);
-  }
+  ];
 
 }
