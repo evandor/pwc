98f03c50b7faf007f46f8744f0955811afde2afe
IzzyGray <mira@skysail.io>
2017-02-28 18:13:40 +0100

    wip

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -16,7 +16,7 @@ import * as moment from 'moment';
 export class AddPage {
 
   reading: Reading = new Reading();
-  readings: Array<Reading> = new Array();
+  //readings: Array<Reading> = new Array();
   addWeightFormGroup: FormGroup;
   weight: any;
   overwrite: any;
@@ -46,29 +46,34 @@ export class AddPage {
     this.reading.date = this.addWeightFormGroup.value.date;
 
     //get already stored objects from storage
-    this.storage.get("readings").then((readings) => {
+    this.storage.get("readings").then((result) => {
+
+      var collisionIndex = null;
+
       //go through existing objects to check if entry for the entered date already exists
-      for (var i = 0; i < readings.length; i++) {
+      for (var i = 0; i < result.length; i++) {
         //if entry already exists aks if entry should be replaced
-        if (moment(readings[i].date).format('YYYY MM DD') == moment(this.reading.date).format('YYYY MM DD')) {
-          this.showConfirm();
-          //entry should be replaced
-          if (this.overwrite) {
-            readings.splice(i, 1, this.reading);
-          }
+        if (moment(result[i].date).format('YYYY MM DD') == moment(this.reading.date).format('YYYY MM DD')) {
+          collisionIndex = i;
+          break;
         }
       }
-      if (this.overwrite == undefined) {
-        readings.push(this.reading);
-      }
 
-      readings.sort((r1, r2) => (r1.date.localeCompare(r2.date)));
-      this.storage.set("readings", readings).then(() => { this.navCtrl.push(ChartsPage) });
+      console.log("Collision Index", collisionIndex);
+
+      if (collisionIndex != null) {
+        this.showConfirm(collisionIndex, result);
+      } else {
+        console.log("business as usual");
+        result.push(this.reading);
+        result.sort((r1, r2) => (r1.date.localeCompare(r2.date)));
+        this.storage.set("readings", result).then(() => { this.navCtrl.push(ChartsPage) });
+      }
 
     });
   }
 
-  showConfirm() {
+  showConfirm(index: number, readings: Array<Reading>) {
     let confirm = this.alertCtrl.create({
       message: 'Do you wish to overwrite existing entry?',
       buttons: [
@@ -76,15 +81,18 @@ export class AddPage {
           text: 'No',
           handler: () => {
             console.log('Disagree clicked');
-            this.overwrite = false;
-
+            this.navCtrl.push(ChartsPage);
           }
         },
         {
           text: 'Yes',
           handler: () => {
             console.log('Agree clicked');
-            this.overwrite = true;
+            console.log("Reading", readings);
+            var deletedItem = readings.splice(index, 1, this.reading);
+            console.log("DEL: " + deletedItem);
+            console.log("Reading", readings);
+            this.storage.set("readings", readings).then(() => { this.navCtrl.push(ChartsPage) });
           }
         }
       ]
