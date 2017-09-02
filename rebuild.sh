ba82143d40c20adf888de3168b967087688b2cee
Carsten Gräf <evandor@gmail.com>
2017-09-02 13:35:56 +0200

    adjusted rebuild.sh script

--- rebuild.sh
+++ rebuild.sh
@@ -3,8 +3,8 @@ rm -rf node_modules/ .tmp/ plugins/ platforms/ www/
 mkdir www
 
 npm install
-ionic platform add ios
+ionic cordova platform add ios
 
 ionic build 
-ionic build ios
+ionic cordova build ios
 
